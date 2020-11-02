5fe3967fc84f155d3ef6832a1ef93da919f1c4eb
arily <lbsaaaaa@gmail.com>
2020-11-02 15:53:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const defaultOptions = {
-  catch: ['unhandledRejection', 'uncaughtException'],
+  catch: ['unhandledRejection'],
   send: {
     private: [],
     group: []
@@ -15,7 +15,7 @@ module.exports.v1 = {
   apply(ctx, options){
     if(installed) return
     
-    options = { ...defaultOptions, options }
+    options = { ...defaultOptions, ...options }
     if (!options.send.private) options.send.private = []
     if (!options.send.group) options.send.group = []
 
@@ -38,7 +38,7 @@ module.exports.v2 = {
   apply(ctx, options){
     if(installed) return
 
-    options = { ...defaultOptions, options }
+    options = { ...defaultOptions, ...options }
     if (!options.send.private) options.send.private = []
     if (!options.send.group) options.send.group = []
 
@@ -48,12 +48,12 @@ module.exports.v2 = {
     const sendGroup = (message) =>  options.send.group.map(id => bot.sendGroupMsg(id, message.toString()))
 
     if (options.catch.includes('unhandledRejection')) process.on('unhandledRejection', (reason, promise) => {
-      handler(`${reason}\n${promise}`, sendPrivate)
-      handler(`${reason}\n${promise}`, sendGroup)
+      handler(`${reason.stack}`, sendPrivate)
+      handler(`${reason.stack}`, sendGroup)
     })
     if (options.catch.includes('uncaughtException')) process.on('uncaughtException', (err, origin) => {
-      handler(`${err}`, sendPrivate)
-      handler(`${err}`, sendGroup)
+      handler(`${err.stack}`, sendPrivate)
+      handler(`${err.stack}`, sendGroup)
     })
     installed = true
   }
